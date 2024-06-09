cef15ac64e8e407d9361fb458f5085fd88e12a9d
igorakimy <igorakimy@gmail.com>
2024-06-09 17:13:54 +0300

    add pattern confinement lexical

---
+++ patterns/confinement/lexical/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	results := chanOwner()
+	consumer(results)
+}
+
+func chanOwner() <-chan int {
+	results := make(chan int)
+	go func() {
+		defer close(results)
+
+		for i := 0; i <= 5; i++ {
+			results <- i
+		}
+	}()
+	return results
+}
+
+func consumer(results <-chan int) {
+	for result := range results {
+		fmt.Printf("Received: %d\n", result)
+	}
+	fmt.Println("Done receiving")
+}
